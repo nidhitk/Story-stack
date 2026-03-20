25b85921a5f406e19b57a0c9c927f698b8a04bef
NIDHEESH TK <nidheeshnidi111@gmail.com>
2026-03-20 16:05:01 +0530

    changes in indentation

--- app/main.py
+++ app/main.py
@@ -1,22 +1,5 @@
 from fastapi import FastAPI
 from app.schemas import ParentContent,ChildContent,postupdate,titleupdate
-app=FastAPI()
-
-# @app.get("/item/{num}")
-# def storystack(n:int):
-#     return {"result":"hello users ,from story stack{num}"}
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str, q: str | None = None, short: bool = False):
-#     item = {"item_id": item_id}
-    
-#     if q:
-#         item.update({"q": q})
-#     if not short:
-#         item.update(
-#             {"description": "This is an amazing item that has a long description"}
-#         )
-#     return item
 from app.database import SessionLocal, engine
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
@@ -24,11 +7,10 @@ from app.models import Content,Posts
 from app import models
 models.Base.metadata.create_all(bind=engine)
 
-
+app=FastAPI()
 @app.get("/")
 def health():
     return {"health":"server is good"}
-
 
 
 def get_db():
